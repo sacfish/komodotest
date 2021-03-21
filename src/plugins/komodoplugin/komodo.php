ed926551be2cd42b8c98802af88829e2fe468dd1
sacfish <isaacelgie@gmail.com>
2021-03-21 17:01:37 +0000

    plugin creates shortcode

--- src/plugins/komodoplugin/komodo.php
+++ src/plugins/komodoplugin/komodo.php
@@ -8,3 +8,14 @@ Version: 0.1
 Author: Isaac Elgie
 Author URI: 
 */
+
+function komodo_test_plugin() {	 
+    return '<div class="komodo-form-wrapper"><input type="text" placeholder="Name">
+    <input type="text" placeholder="Address">
+    <input type="email" placeholder="Email">
+    <input type="tel" placeholder="Telephone">
+    <textarea placeholder="Message..." rows="5"></textarea>
+    <button type="submit" href="/">Submit</button></div>';
+}
+
+add_shortcode('komodo', 'komodo_test_plugin');
